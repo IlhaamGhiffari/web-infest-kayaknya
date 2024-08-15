80cee796fee665986b7a8a72ca2d227255c6ff51
glennhkm <glenn@mhs.usk.ac.id>
2024-08-16 00:35:10 +0700

    fix: navigation hero swiper and logo infest

--- src/app/buku-panduan/page.tsx
+++ src/app/buku-panduan/page.tsx
@@ -5,7 +5,7 @@ const BukuPanduan = () => {
   return (
     <div className='w-screen h-screen flex flex-col justify-center items-center gap-14 relative'>
         <Image
-            src='https://jbymoblt3fzt0jez.public.blob.vercel-storage.com/logo-infest-usk.webp'
+            src='/assets/images/logo-infest-usk.png'
             width={500}
             height={500}
             className='w-1/12 absolute top-[20%]'
