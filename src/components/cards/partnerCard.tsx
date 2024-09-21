f1fa19ecfb817d7d2445d2ec1e3c43e1a4ca4d36
glennhkm <glenn@mhs.usk.ac.id>
2024-09-21 11:33:26 +0700

    style: add white-bg on partners logo

--- src/components/cards/partnerCard.tsx
+++ src/components/cards/partnerCard.tsx
@@ -11,13 +11,15 @@ interface PartnerCardProps {
 export const PartnerCard = (props: PartnerCardProps) => {
   return (
     <div className="lg:w-60 lg:h-32 w-44 h-24 mx-2 my-8 p-2 rounded-xl shadow-lg shadow-black border-[0.5px] bg-secondary-yellow/5 border-primary-yellow flex justify-center items-center">
-      <Image
-        src={props.logoSrc}
-        alt="logo-partner-infest-usk"
-        width={props.width}
-        height={props.height}
-        className={`rounded-xl ${props.className}`}
-      />
+      <div className={`rounded-xl ${props.className}`}>
+        <Image
+          src={props.logoSrc}
+          alt="logo-partner-infest-usk"
+          width={props.width}
+          height={props.height}
+          className={`rounded-xl`}
+        />
+      </div>
     </div>
   );
 };
